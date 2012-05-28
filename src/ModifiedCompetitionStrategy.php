19754f3809f08bb243c85e32a188141823c48a78
Maarten Claes <maartencls@gmail.com>
2012-05-28 09:01:27 +0200

    More readable ModifiedCompetitionStrategy class

--- src/ModifiedCompetitionStrategy.php
+++ src/ModifiedCompetitionStrategy.php
@@ -1,25 +1,28 @@
 <?php
 /**
  * Modified competition strategy ( 1334 )
+ * Leaves the gaps in the ranking numbers before the sets of equal-ranking 
+ * items (rather than after them as in standard competition ranking)
  */
 class ModifiedCompetitionStrategy extends RankingStrategy {
 
-  private $equally_ranked = array();
+  private $equalRanking = array();
 
   protected function whenEqual($rankable, $ranking_index) {
-    $this->equally_ranked[] = $rankable;
-    $this->updateEquallyRanked($ranking_index);
+    $this->updateEqualRankingItems($rankable, $ranking_index);
   }
   
-  private function updateEquallyRanked($ranking_index) {
-    foreach ($this->equally_ranked as $r) {
+  private function updateEqualRankingItems($rankable, $ranking_index) {
+    $this->equalRanking[] = $rankable;
+    // ranking = value after gap = $ranking_index + 1
+    foreach ($this->equalRanking as $r) {
       $r->ranking = $ranking_index + 1;
     }
   }
 
   protected function whenDifferent($rankable, $ranking_index) {
     $rankable->ranking = $ranking_index + 1;
-    $this->equally_ranked = array($rankable);
+    $this->equalRanking = array($rankable);
   }
 
 }
